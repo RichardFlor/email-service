a2d06a162ae9df8f4c44dc14bc3a16eb047d023b
RichardFlor <richardflor2015@gmail.com>
2025-01-25 13:24:24 -0300

    email service aws

--- src/main/java/com/richard/email_service/EmailServiceApplication.java
+++ src/main/java/com/richard/email_service/EmailServiceApplication.java
@@ -13,24 +13,25 @@ public class EmailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
-		// Inicializando o cliente do Resend com a chave de API
-		Resend resend = new Resend("re_GcS9bnUT_FX7cdubFP244U3ZaCtFYYg7Y");
 
-		// Definindo os parâmetros do e-mail
-		CreateEmailOptions params = CreateEmailOptions.builder()
-				.from("Acme <onboarding@resend.dev>")
-				.to("duckieanki@gmail.com")
-				.subject("aws lixo!")
-				.html("<strong>Deu bom </strong>")
-				.build();
-
-		try {
-			// Enviando o e-mail
-			CreateEmailResponse data = resend.emails().send(params);
-			System.out.println("E-mail enviado com ID: " + data.getId());
-		} catch (ResendException e) {
-			// Tratamento de exceções
-			e.printStackTrace();
-		}
+//		// Inicializando o cliente do Resend com a chave de API
+//		Resend resend = new Resend("re_GcS9bnUT_FX7cdubFP244U3ZaCtFYYg7Y");
+//
+//		// Definindo os parâmetros do e-mail
+//		CreateEmailOptions params = CreateEmailOptions.builder()
+//				.from("Acme <onboarding@resend.dev>")
+//				.to("duckieanki@gmail.com")
+//				.subject("aws lixo!")
+//				.html("<strong>Deu bom </strong>")
+//				.build();
+//
+//		try {
+//			// Enviando o e-mail
+//			CreateEmailResponse data = resend.emails().send(params);
+//			System.out.println("E-mail enviado com ID: " + data.getId());
+//		} catch (ResendException e) {
+//			// Tratamento de exceções
+//			e.printStackTrace();
+//		}
 	}
 }
